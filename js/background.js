2cae32bfc0e7082db210a9320238e94c788f27c0
parkmincheol <maxandy@naver.com>
2022-04-15 14:10:22 +0900

    background.js 수정

--- js/background.js
+++ js/background.js
@@ -8,5 +8,5 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-//[quiz] 'append()' vs 'appendChild()' 둘 다 시도해보기
+//[quiz] 'append()' vs 'appendChild()' 둘 다 시도해보기 
 document.body._____(bgImage);
