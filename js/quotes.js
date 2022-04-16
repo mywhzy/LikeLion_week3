3f9105218e11ac1a0d3d2f1fbe3df434589c780c
parkmincheol <maxandy@naver.com>
2022-04-16 19:10:16 +0900

    quotes.js 퀴즈 삭제

--- js/quotes.js
+++ js/quotes.js
@@ -42,10 +42,10 @@ const quotes = [
   },
 ];
 
-//[quiz] 'css 가상클래스'를 참고하여 id="quote"의 처음 글자 요소 가져오기
-const quote = document.querySelector("_quote ___:_____-_____");
-//[quiz] 'css 가상클래스'를 참고하여 id="quote"의 마지막 글자 요소 가져오기
-const author = document.querySelector("_quote ___:_____-_____");
+//css 가상클래스 'first-child' id="quote"의 처음 글자 요소(span) 가져오기
+const quote = document.querySelector("#quote span:first-child");
+//css 가상클래스 'last-child' id="quote"의 마지막 글자 요소(span) 가져오기
+const author = document.querySelector("#quote span:last-child");
 //todaysQuote에 인용구를 랜덤으로 선언
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
